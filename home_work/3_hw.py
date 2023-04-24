5dc7b447757e8469bd28392419a53b76d4076ba6
NastyaSiya <rumyantseva11aa@gmail.com>
2023-04-24 23:51:31 +0300

    ,,

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -1,63 +1,78 @@
-a = 10
-b = 20
+def z1(a, b):
 
-if a > b:
-    print(a)
-elif a == b:
-    print(a)
-else:
-    print(b)
-
-c = 100
-d = 236
-
-if c - d == 135 or c - d == -135:
-    print('yes')
-else:
-    print('No')
-
-m = 5
-
-if m == 12 or m == 1 or m == 2:
-    print('zima')
-elif m == 3 or m == 4 or m == 5:
-    print('vesna')
-elif m == 6 or m == 7 or m == 8:
-    print('leto')
-elif m == 9 or m == 10 or m == 11:
-    print('osen')
-
-k = 20
-d = 30
-t = 10
-
-if k > 10 and d > 10 and t > 10:
-    print('yes')
-else:
-    print('no')
-
-q = [1, -4, -3, 8, -256]
-n = 0
-i = 0
-
-for i in range(5):
-    if 0 < q[i]:
-        n = n + 1
-print(n)
-
-year = 4
-month = 3
-
-print((year*12 + month)*29)
+    if a > b:
+        return a
+    elif a == b:
+        return a
+    else:
+        return b
 
 
+print(z1(10, 20))
+
+
+def z2(c, y):
+
+    if c - y == 135 or c - y == -135:
+        return 'yes'
+    else:
+        return 'No'
+
+
+print(z2(100, 236))
+
+
+def z3(m):
+    if m == 12 or m == 1 or m == 2:
+        return 'zima'
+    elif m == 3 or m == 4 or m == 5:
+        return 'vesna'
+    elif m == 6 or m == 7 or m == 8:
+        return 'leto'
+    elif m == 9 or m == 10 or m == 11:
+        return 'osen'
+
+
+print(z3(5))
+
+
+def z4(k, d, t):
+    if k > 10 and d > 10 and t > 10:
+        return 'yes'
+    else:
+        return 'no'
+
+
+print(z4(220, 30, 10))
+
+
+def z5(q, n, i):
+    for i in range(5):
+        if 0 < q[i]:
+            n = n + 1
+        return n
+
+
+print(z5([1, -4, -3, 8, -256], 0, 0))
+
+
+def z6(year, month):
+    return (year*12 + month)*29
+
+
+print(z6(4, 3))
 zvanie = 5
 
-if 1 <= zvanie <= 4:
-    print('bakalavr')
-elif 5 <= zvanie <= 6:
-    print('magistr')
-elif 7 <= zvanie <= 9:
-    print('aspirant')
-else:
-    print('Vvedite korrektniy god obucheniya')
+
+def z6(g):
+    if 1 <= g <= 4:
+        return 'bakalavr'
+    elif 5 <= g <= 6:
+        return 'magistr'
+    elif 7 <= g <= 9:
+        return 'aspirant'
+    else:
+        return 'Vvedite korrektniy god obucheniya'
+
+
+print(z6(5))
